07925eb40b9a50b77302bebe85025a605208e312
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-19 09:52:11 +0300

    commit tree f0b527f50ca186529c050de292bb371e5f67a010

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,29 @@
 #write your code here
 
-def countdown
+def countdown n
+    #  puts "#{n} SECOND(S)!"
+    
+    while n>0
+     puts "#{n} SECOND(S)!"
+    #  sleep(1)     
+     n-=1
+    end
+    # puts "HAPPY NEW YEAR!" 
+    return "HAPPY NEW YEAR!" 
 end
+# countdown 10
+
+
+def countdown_with_sleep n
+    
+    while n>0
+     puts "#{n} SECOND(S)!"
+    sleep(1)   
+     n-=1
+    end
+    puts "HAPPY NEW YEAR!" 
+    return "#{n} SECOND(S)!"
+
+    
+end
+# countdown_with_sleep 
